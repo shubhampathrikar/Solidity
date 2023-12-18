820ebfd80fd5af015ddf55c657489dcf1c5f2104
Shubham Pathrikar <shubhampathrikar07@gmail.com>
2023-12-18 22:37:41 +0530

    First contract part 2

--- basics/HotelRoom.sol
+++ basics/HotelRoom.sol
@@ -11,6 +11,8 @@ contract HotelRoom{
     enum Statuses { Vacant, Occupied }
     Statuses currentStatus;
 
+    event Occupy(address _occupant, uint _value);
+
     address payable public owner;
 
     constructor(){
@@ -18,9 +20,20 @@ contract HotelRoom{
         currentStatus = Statuses.Vacant;
     }
 
+    modifier onlyWhileVacant {
+        require(currentStatus == Statuses.Vacant,"Currently occupied.");
+        _;
+    }
 
-    function book() public payable {
-        owner.transfer(msg.value);
+    modifier costs(uint _amount){
+        require(msg.value >= _amount,"Not enough ether provided");
+        _;
+    }
+
+    function book() public payable onlyWhileVacant costs(2 ether){
         currentStatus = Statuses.Occupied;
+        owner.transfer(msg.value);
+        
+        emit Occupy(msg.sender, msg.value);
     }
 }
