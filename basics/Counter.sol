db600b7e1a350242cedc2a076d06f954912ac6b5
Shubham Pathrikar <shubhampathrikar07@gmail.com>
2023-12-24 13:37:50 +0530

    Updating counter contract

--- basics/Counter.sol
+++ basics/Counter.sol
@@ -3,16 +3,33 @@ pragma solidity ^0.8.0;
 
 contract Counter{
     uint count; // unsigned integer
+    string name;
 
     constructor() {
         count = 0;
+        name = "Shubham";
     }
 
     function getCount() public view returns(uint) {
         return count;
     }
 
-    function incrementCount() public {
+    function incrementCount() public returns (uint newCount) {
         count++;
+        return count;
+    }
+
+    function decrementCount() public returns (uint newCount) {
+        count--;
+        return count;
+    }
+
+    function getName() public view returns(string memory currentName){
+        return name;
+    }
+
+    function setName(string memory _newName) public returns (string memory newName) {
+        name = _newName;
+        return name;
     }
 }
